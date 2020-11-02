6eed7d1316f93761e1a8c57354a7a4bc0cf07c20
hellc <kh03401@gmail.com>
2020-11-02 18:39:18 +0800

    initial file lru.c 	done data structure 	not algo yet 	hellc wang

---
+++ lru.c
@@ -0,0 +1,45 @@
+/*****************************
+ * This program implement lru algorithm
+ * Use hashmap + double linked list to inplement
+ *
+ * hellc
+ * 28102020
+ *****************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+/**********************
+ * MAX_SIZE is the max size of memory
+ * Can alternative eazy
+ *********************/
+#define MAX_SIZE 500
+
+/***************************
+ * key = memory block name
+ * value = address of key in linked list
+ * next point to next struct of map
+ *****************************/
+typedef struct Hash{
+	int key;
+	long long value;
+	struct Hash *next;
+}map;
+
+/*****************************
+ * Node of "double" linked list
+ * Main value is blockname
+ * Fast to mantain
+ ****************************/
+typedef struct Node{
+	struct Node *pre;
+	int blockname;
+	struct Node *next;
+}node;
+
+void main(void){
+	map hmap[MAX_SIZE];
+	int i = 0;
+	node 
+
+	
+
