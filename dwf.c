4e3bfb9593a4ad7dc3f04df7d496578d1a1f2cd3
hellcwang <kh03401@gmail.com>
2020-12-31 04:29:16 +0800

    Add some note to explain dwf.c

--- dwf.c
+++ dwf.c
@@ -12,17 +12,28 @@ int main(void){
                 return -1;
         }
 
+	//Use to map for to type of memory
+	//Need to indepdent linked list
+	//So there is too head and tail
 	map **dram, **pram;
         node *d_head = NULL;
         node *p_head = NULL;
         node *d_tail = NULL;
         node *p_tail = NULL;
+
+	//Use index to represent the order
 	int index = 0;
+
+	//Determine if over max size or not
 	int p_count = 0;
 	int d_count = 0;
+
+	//tmprary carry index blockname from dram to pram
 	int tmp_index;
 	int tmp_b_name;
 	int line = 0;
+
+	//Deleted block name
 	int d_b_name;
 	
 	hmap_init(&dram);
@@ -35,9 +46,13 @@ int main(void){
 		if(block_find(b_name, dram) == NULL && block_find(b_name, pram) == NULL){
 			//printf("miss ");
 			if(read == 0){
+				
+				//DRAM is full
 				if(d_count++ >= MAX_SIZE){
 					//printf("d_over ");
 
+					//If no node in PRAM
+					//U can put node from DRAM to PRAM directly
 					if(p_head== NULL){
 						//printf("d->p ");
 						//move from dram to pram
@@ -46,11 +61,16 @@ int main(void){
 						d_b_name = block_delete(&d_tail);
 						hmap_delete(d_b_name, dram);
 						hmap_add(tmp_b_name, block_add(tmp_b_name, &p_head, &p_tail, tmp_index), pram);
+
+					// Compare if invicted of DRAM is younger than head of PRAM
+					// Yes -> put it to head of PRAM
 					}else if(p_head != NULL && d_tail->index < p_head->index){
 						//printf("d->p ");
 						//move from dram to pram
 						tmp_b_name = d_tail->blockname;
 						tmp_index = d_tail->index;
+
+						//If PRAM is full
 						if(p_count >= MAX_SIZE){
 							d_b_name = block_delete(&p_tail);
 							hmap_delete(d_b_name, pram);
@@ -58,6 +78,7 @@ int main(void){
 						d_b_name = block_delete(&d_tail);
 						hmap_delete(d_b_name, dram);
 						hmap_add(tmp_b_name, block_add(tmp_b_name, &p_head, &p_tail, tmp_index), pram);
+					//Otherwise just delete
 					}else{
 						d_b_name = block_delete(&d_tail);
 						hmap_delete(d_b_name, dram);
@@ -66,6 +87,7 @@ int main(void){
 				}
 				hmap_add(b_name, block_add(b_name, &d_head, &d_tail, index++), dram);
 			}
+			//Put read in PRAM to protect PRAM
 			else if(read == 1){
 				if(p_count++ >= MAX_SIZE){
 					d_b_name = block_delete(&p_tail);
@@ -74,8 +96,9 @@ int main(void){
 				}
 				hmap_add(b_name, block_add(b_name, &p_head, &p_tail, index++), pram);
 			}
+
+		//Cache hit
 		}else{
-			//printf("hit ");
 			if(block_find(b_name, dram) != NULL){
 			       block_alter(block_find(b_name, dram), &d_head, &d_tail);
 			}else if(block_find(b_name, pram) != NULL){
